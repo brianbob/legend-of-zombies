bbdd7aba67a2550e0aa900b9f7a4813ab208744b
Brian Bridge <brianjbridge@gmail.com>
2015-05-17 17:28:24 -0400

    zombies!

--- js/helper_functions.js
+++ js/helper_functions.js
@@ -109,6 +109,12 @@ Zombie = function (index, game, player) {
   this.health = 3;
   this.player = player;
   this.alive = true;
+  
+  this.zombie = game.add.sprite(x, y, 'zombies');
+  this.zombie.animations.add('left', [12, 13, 14], 10, true);
+  this.zombie.animations.add('right', [24, 25, 26], 10, true);
+  this.zombie.animations.add('down', [0, 1, 2], 10, true);
+  this.zombie.animations.add('up', [36, 37, 38], 10, true);
 
   // I have no idea what this does.
   this.zombie.anchor.set(0.5);
@@ -118,12 +124,6 @@ Zombie = function (index, game, player) {
   this.zombie.body.immovable = false;
   this.zombie.body.collideWorldBounds = true;
   this.zombie.body.bounce.setTo(1, 1);
-  
-  this.zombie = game.add.sprite(x, y, 'zombies');
-  this.zombie.animations.add('left', [12, 13, 14], 10, true);
-  this.zombie.animations.add('right', [24, 25, 26], 10, true);
-  this.zombie.animations.add('down', [0, 1, 2], 10, true);
-  this.zombie.animations.add('up', [36, 37, 38], 10, true);
 
   //this.zombie.angle = game.rnd.angle();
   //game.physics.arcade.velocityFromRotation(this.tank.rotation, 100, this.tank.body.velocity);
